89900355c562821d1c47d374ba2c884753167f2f
Sy Le <lenguyensy@gmail.com>
2021-09-17 05:28:37 -0700

    fixed minor issue with missing deps for usecallback

--- index.jsx
+++ index.jsx
@@ -723,7 +723,7 @@ document.addEventListener('AppCopyTextToClipboard', (e) => window.copyToClipboar
 
       //update the cache in the session storage
       _persistBufferSchema(bufferSchema); // commit the changes
-    }, []);
+    }, [bufferSchema]);
 
     const onCancel = useCallback(async () => {
       if (hasPendingChanges) {
@@ -739,9 +739,9 @@ document.addEventListener('AppCopyTextToClipboard', (e) => window.copyToClipboar
     }, []);
 
     const onTest = useCallback(() => {
-      const base64URL = _getNavBookmarkletFromSchema(schema);
+      const base64URL = _getNavBookmarkletFromSchema(bufferSchema);
       _navigateToDataUrl(base64URL, true);
-    }, []);
+    }, [bufferSchema]);
 
     const onSetBufferSchema = useCallback((newBufferSchema) => {
       setBufferSchema(newBufferSchema);
