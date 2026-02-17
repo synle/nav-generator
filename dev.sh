b883b36c9e20fb527f524f139b4fa840f9768dfd
Sy Le <lenguyensy@gmail.com>
2026-02-17 07:42:45 -0800

    minor code update for npm run start

--- dev.sh
+++ dev.sh
@@ -1,1 +1,1 @@
-curl -s -- https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/dev.sh | bash -s -- '*.jsx *.scss'
+curl -s -- https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/dev.sh | bash -s -- '*.jsx *.scss' 'npm run start'
