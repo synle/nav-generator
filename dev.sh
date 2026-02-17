99f863c867418c57e994857bd9773c95224b01d6
Sy Le <lenguyensy@gmail.com>
2026-02-17 07:28:14 -0800

    make the dev script more robust

--- dev.sh
+++ dev.sh
@@ -1,18 +1,25 @@
-npm i
+# If $1 is provided, use it. Otherwise default to *.jsx *.scss
+FILE_TO_WATCH=${1:-"*.jsx *.scss"}
 
-# Get last modification time and file size of source files in root folder
 get_file_state() {
- stat -f "%m %z" *.jsx *.scss 2>/dev/null | sort -r
+  # Expand glob patterns properly
+  stat -f "%m %z" $FILE_TO_WATCH 2>/dev/null | sort -r
 }
 
+# do the first build
+npm i
 sh build.sh
 
-LAST_STATE=$(get_file_state)
-while sleep 3; do
- CURRENT_STATE=$(get_file_state)
- if [ "$CURRENT_STATE" != "$LAST_STATE" ]; then
-   sh build.sh
-   LAST_STATE=$CURRENT_STATE
- fi
-done
+# start the http-server in the background
+npx http-server . > /dev/null
 
+LAST_STATE=$(get_file_state)
+
+while sleep 3; do
+  CURRENT_STATE=$(get_file_state)
+
+  if [ "$CURRENT_STATE" != "$LAST_STATE" ]; then
+    sh build.sh
+    LAST_STATE="$CURRENT_STATE"
+  fi
+done
