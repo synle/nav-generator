e74d3e268621bcf992be3c20e42f2fcdba224ad2
Sy Le <lenguyensy@gmail.com>
2026-02-17 07:44:16 -0800

    minor update to the dev script

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
-curl -s -- https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/dev.sh | bash -s -- '*.jsx *.scss' 'npm run start'
+curl -s -- https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/dev.sh | \
+bash -s -- '*.json *.scss *.jsx *.js' 'npm run start'
